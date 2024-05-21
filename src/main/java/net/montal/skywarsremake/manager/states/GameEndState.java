9dc887bdad17ac3cf39364ca73eb07ae7099a519
Zoridon <avoleuss@gmail.com>
2024-05-20 20:44:11 -0400

    Added GameEndState method

--- src/main/java/net/montal/skywarsremake/manager/states/GameEndState.java
+++ src/main/java/net/montal/skywarsremake/manager/states/GameEndState.java
@@ -4,4 +4,18 @@ import net.montal.skywarsremake.manager.State;
 
 public class GameEndState extends State {
 
+    @Override
+    public void onStart() {
+        
+    }
+
+    @Override
+    public void onEnd() {
+
+    }
+
+    @Override
+    public boolean readyToEnd() {
+        return false;
+    }
 }
