9398a055de41274c826aeb081d8bbc0962696939
Zoridon <avoleuss@gmail.com>
2024-05-21 17:40:16 -0400

    Updated the deathlistener

--- src/main/java/net/montal/skywarsremake/events/DeathListener.java
+++ src/main/java/net/montal/skywarsremake/events/DeathListener.java
@@ -4,6 +4,8 @@ import net.montal.skywarsremake.manager.GameManager;
 import net.montal.skywarsremake.manager.SkywarsGame;
 import net.montal.skywarsremake.object.GameState;
 import net.montal.skywarsremake.object.GameStateManager;
+import net.montal.skywarsremake.utils.CC;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,7 +21,11 @@ public class DeathListener implements Listener {
         Player user = e.getEntity();
 
         SkywarsGame game = GameManager.getGameForPlayer(user.getPlayer());
-        if (game)
+        if (GameManager.isState(GameState.ACTIVE)) {
+            e.setDeathMessage(CC.translate("&a" + user.getName() + " &ahas died!"));
+            user.setGameMode(GameMode.SPECTATOR);
+            user.sendMessage(CC.translate("&aYou have died!"));
+        }
 
     }
 
